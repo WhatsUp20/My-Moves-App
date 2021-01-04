a0f42ee288e411325ab36335a1e41b95fbccc833
Alex Nazarov <storyofspolding@gmail.com>
2021-01-04 22:28:22 +0300

    part8 (add ability to watch trailer and review)

---
+++ app/src/main/java/com/example/mymovies/adapters/TrailerAdapter.java
@@ -0,0 +1,71 @@
+package com.example.mymovies.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mymovies.R;
+import com.example.mymovies.data.Trailer;
+
+import java.util.List;
+
+public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.TralerViewHolder> {
+
+    List<Trailer> trailers;
+
+    public void setTrailers(List<Trailer> trailers) {
+        this.trailers = trailers;
+        notifyDataSetChanged();
+    }
+    private OnTrailerClickListener listener;
+
+    public void setListener(OnTrailerClickListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnTrailerClickListener {
+        void onClick(String url);
+    }
+
+    @NonNull
+    @Override
+    public TralerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.trailer_item,parent,false);
+        return new TralerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TralerViewHolder holder, int position) {
+        Trailer trailer = trailers.get(position);
+        holder.textViewNameOfVideo.setText(trailer.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return trailers.size();
+    }
+
+    class TralerViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewNameOfVideo;
+
+        public TralerViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewNameOfVideo = itemView.findViewById(R.id.textViewNameOfVideo);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onClick(trailers.get(getAdapterPosition()).getKey());
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/mymovies/screens/FavouriteActivity.java
+++ app/src/main/java/com/example/mymovies/screens/FavouriteActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.example.mymovies.adapters.MovieAdapter;
 import com.example.mymovies.data.FavoriteMovie;
 import com.example.mymovies.data.MainViewModel;
 import com.example.mymovies.data.Movie;
